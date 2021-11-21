325c1d065f04a05502dc39539db256fc634c455f
Akul Srivastava <akulsr0@gmail.com>
2021-11-21 12:16:11 +0530

    chore: dev tips

---
+++ content/dev-tips/vscode/01-emoji-in-vscode.md
@@ -0,0 +1,23 @@
+---
+title: Emoji in VSCode
+category: vscode
+---
+
+![emoji-in-vscode](https://user-images.githubusercontent.com/43666833/142738239-ce45a585-a92f-4cf4-be54-b6e460476b96.gif)
+
+<br />
+
+There could be cases where you wish to add emoji in your webpage, mobile application user interface, etc.
+
+You can easily add the emoji within VSCode instead of going to the browser and getting it from there.
+
+<br />
+
+**Steps to add Emoji in VSCode**
+
+1. Go to **Edit**
+2. Choose **Emoji and Symbols**
+
+or
+
+1. MacOS: Press ctrl + cmd + space
---
+++ pages/dev-tips/[category]/[tip].tsx
@@ -0,0 +1,85 @@
+import { useEffect, useRef } from "react";
+import { GetStaticPropsContext, NextPage } from "next";
+import matter from "gray-matter";
+import marked from "marked";
+import path from "path";
+import fs from "fs";
+
+import Container from "../../../components/Container";
+import Footer from "../../../components/Footer";
+import Header from "../../../components/Header";
+import Head from "../../../components/_head";
+
+interface ITip {
+  data: {
+    title: string;
+    category: string;
+  };
+  content: string;
+}
+
+interface DevTipProps {
+  tip: ITip;
+}
+
+const DevTip: NextPage<DevTipProps> = (props) => {
+  const title = props?.tip?.data.title || "";
+  const content = props?.tip?.content;
+  const tipContentRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    content &&
+      tipContentRef.current &&
+      (tipContentRef.current.innerHTML += marked(content));
+  }, [content]);
+
+  return (
+    <Container>
+      <Head title={title} />
+      <Header />
+      <br />
+      <h3>{title}</h3>
+      <div ref={tipContentRef}>
+        <br />
+      </div>
+      <Footer />
+    </Container>
+  );
+};
+
+export async function getStaticProps(ctx: GetStaticPropsContext) {
+  const params = ctx.params!;
+  const { category, tip } = params;
+
+  const tipsPath = path.join(`content/dev-tips/${category}`);
+  const tips = fs.readdirSync(tipsPath);
+  const _tip = tips.find((b) => b.toLowerCase().includes(tip as string))!;
+  const tipContent = fs.readFileSync(
+    path.join(`content/dev-tips/${category}/${_tip}`),
+    "utf-8"
+  );
+  const { data, content } = matter(tipContent);
+
+  return { props: { tip: { data, content } } };
+}
+
+export async function getStaticPaths() {
+  const paths: Array<{ params: { category: string; tip: string } }> = [];
+
+  const devTipsCategoriesPath = path.join("content/dev-tips");
+  const devTipsCategories = fs.readdirSync(devTipsCategoriesPath);
+  devTipsCategories.forEach((category) => {
+    const devTips = fs.readdirSync(`content/dev-tips/${category}`);
+    devTips.forEach((tip) => {
+      const path = { params: { category, tip } };
+      paths.push(path);
+    });
+  });
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export default DevTip;
