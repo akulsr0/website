83d5c8d77eef6d16f0be1035546503a08268d49a
Akul Srivastava <akulsr0@gmail.com>
2022-03-30 14:43:25 +0530

    feat: added view more in dev tips

---
+++ prebuild.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const path = require("path");
+const matter = require("gray-matter");
+
+const devTips = getDevTips();
+writeDevTipsJSON(JSON.stringify(devTips));
+
+function writeDevTipsJSON(tips) {
+  const filePath = path.join("content/devtips.json");
+  fs.writeFileSync(filePath, `{"devTips": ${tips}}`);
+}
+
+function getDevTips() {
+  const contentPath = path.join("content/dev-tips");
+  const contents = fs.readdirSync(contentPath);
+  const devTips = [];
+
+  for (let content of contents) {
+    const _cPath = path.join(contentPath, content);
+    const _devTips = fs.readdirSync(_cPath);
+    _devTips.forEach((tip) => {
+      const tipDate = matter(
+        fs.readFileSync(path.join(contentPath, content, tip), "utf-8")
+      ).data.date;
+      devTips.push({
+        category: content,
+        date: tipDate,
+        tip: tip.split("-").slice(1).join("-").replace(/.md/, ""),
+      });
+    });
+  }
+
+  devTips.sort((a, b) => Date.parse(b.date) - Date.parse(a.date));
+  return devTips;
+}
