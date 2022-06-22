506c9e25bc6241da6a362ecb7e1fb8d1cfe29d2b
Akul Srivastava <akulsr0@gmail.com>
2022-06-22 21:25:02 +0530

    feat: get oss data from gh api

---
+++ interfaces/API.ts
@@ -0,0 +1,7 @@
+interface GetPullRequestsOptions {
+  org: string;
+  repo: string;
+  author?: string;
+}
+
+export type { GetPullRequestsOptions };
--- services/APIService.ts
+++ services/APIService.ts
@@ -1,3 +1,5 @@
+import { GetPullRequestsOptions } from "../interfaces/API";
+
 const DEFAULT_HEADERS = {
   "Content-Type": "application/json",
 };
@@ -14,6 +16,13 @@ class APIService {
       .then((r) => r.json())
       .then((res) => cb(null, res))
       .catch((err) => cb(err, null));
+  }
+
+  getPullRequests(options: GetPullRequestsOptions, cb: CallableFunction) {
+    const { org, repo, author = "akulsr0" } = options;
+    const url = `https://api.github.com/search/issues?q=is:pr+repo:${org}/${repo}+author:${author}`;
+    const _options = { method: "GET" };
+    this.requestSync(url, _options, cb);
   }
 
   sendContactMessage(message: string, cb: CallableFunction) {
