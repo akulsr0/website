c1bac606e3e4ca29d2d3be669f2c1ee02cfa5cb7
Akul Srivastava <akulsr0@gmail.com>
2022-11-26 12:06:15 +0530

    chore: added open sans font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,18 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700&family=Open+Sans:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
