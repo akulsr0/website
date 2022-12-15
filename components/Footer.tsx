c80c2f865a702956b0edbc80d2fffce17a106614
Akul Srivastava <akulsr0@gmail.com>
2022-12-15 19:25:38 +0530

    feat: update footer to v2 (#49)

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,20 +1,10 @@
 import { NextPage } from "next";
+import List from "../components/List";
+import defaults from "../constants/default.json";
 import { useTheme } from "../context/ThemeContext";
 import styles from "../styles/Footer.module.css";
 
-const footerLinks = [
-  { link: "/github", text: "github" },
-  { link: "/instagram", text: "instagram" },
-  { link: "/linkedin", text: "linkedin" },
-  { link: "/twitter", text: "twitter" },
-  { link: "/stackoverflow", text: "stackoverflow" },
-  { link: "https://akulsr0.gumroad.com/", text: "gumroad" },
-  { link: "https://www.npmjs.com/~akulsr0", text: "npm" },
-  {
-    link: "https://play.google.com/store/apps/developer?id=Akul+Srivastava",
-    text: "playstore",
-  },
-];
+const { footer_links: footerLinks } = defaults;
 
 const Footer: NextPage = () => {
   const { isDarkTheme } = useTheme();
@@ -27,52 +17,11 @@ const Footer: NextPage = () => {
         borderTop: `0.25rem solid ${isDarkTheme ? "#9b9786" : "#a5a5a5"}`,
       }}
     >
-      <style jsx>{`
-        small,
-        a {
-          text-decoration: none;
-          color: ${isDarkTheme ? "#ece3cc" : "#222"};
-        }
-        ul {
-          display: flex;
-          flex-flow: row wrap;
-          align-items: center;
-          justify-content: center;
-          list-style: none;
-          column-gap: 0.6rem;
-        }
-        ul a {
-          text-decoration: underline;
-        }
-      `}</style>
-
-      <h4 style={{ color: isDarkTheme ? "#ece3cc" : "#3d3d3d" }}>
-        connect with me
-      </h4>
-
-      <ul>
-        {footerLinks.map(({ link, text }) => (
-          <li key={text}>
-            <a href={link} target="_blank" rel="noreferrer">
-              {text}
-            </a>
-          </li>
+      <section>
+        {Object.values(footerLinks).map((fl) => (
+          <List list={fl.list} title={fl.title} />
         ))}
-      </ul>
-
-      <div>
-        <small>
-          &copy;{" "}
-          <a
-            href="https://github.com/sanesource/next-personal-blog-template"
-            target="_blank"
-            rel="noreferrer"
-          >
-            SaneSource
-          </a>
-          , {new Date().getFullYear()}
-        </small>
-      </div>
+      </section>
     </footer>
   );
 };
