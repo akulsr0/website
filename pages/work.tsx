2929bd947c4414d2fe7f830545d5324edcd917e6
Akul Srivastava <akulsr0@gmail.com>
2022-12-19 22:59:21 +0530

    chore: update work page

--- pages/work.tsx
+++ pages/work.tsx
@@ -1,28 +1,51 @@
 import { NextPage } from "next";
+import { useLayoutEffect, useRef } from "react";
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
-import marked from "marked";
+import moment from "moment";
+import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
+import { serialize } from "next-mdx-remote/serialize";
 
 import Container from "../components/Container";
 import Head from "../components/_head";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
+import { useTheme } from "../context/ThemeContext";
+import styles from "../styles/Work.module.css";
+
 interface WorkProps {
-  workContent: string;
+  workContent: MDXRemoteSerializeResult<
+    Record<string, unknown>,
+    Record<string, string>
+  >;
 }
 
 const Work: NextPage<WorkProps> = (props) => {
   const { workContent } = props;
+  const { isDarkTheme } = useTheme();
+  const expCalloutRef = useRef<HTMLDivElement>();
+
+  useLayoutEffect(() => {
+    const start = moment([2020, 11, 16]);
+    const now = moment();
+    const duration = moment.duration(now.diff(start));
+    const [y, d, h] = [duration.years(), duration.days(), duration.hours()];
+    const exp = `${y} years ${d} days ${h} hours`;
+    expCalloutRef.current!.innerText = exp;
+  }, []);
 
   return (
     <Container>
       <Head title="Work" />
       <Header />
       <article className="main-content">
-        <h2 style={{ marginBottom: "1rem" }}>Work</h2>
-        <div dangerouslySetInnerHTML={{ __html: workContent }} />
+        <h2 style={{ marginBottom: "1rem" }}>Work Experience</h2>
+        <MDXRemote
+          {...workContent}
+          scope={{ styles, isDarkTheme, expCalloutRef }}
+        />
       </article>
       <Footer />
     </Container>
@@ -31,9 +54,10 @@ const Work: NextPage<WorkProps> = (props) => {
 
 export async function getStaticProps() {
   const workContentPath = path.join("content");
-  const workContent = fs.readFileSync(workContentPath + "/Work.md", "utf-8");
+  const workContent = fs.readFileSync(workContentPath + "/Work.mdx", "utf-8");
   const { content } = matter(workContent);
-  return { props: { workContent: marked(content) } };
+  const outputContent = await serialize(content);
+  return { props: { workContent: outputContent } };
 }
 
 export default Work;
