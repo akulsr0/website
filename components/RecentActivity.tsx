4b6533a41dd7e70f46d3b7125b39faa9b39c034c
Akul Srivastava <akulsr0@gmail.com>
2022-12-25 13:34:10 +0530

    refactor: move `RecentActivity` to component

---
+++ components/RecentActivity.tsx
@@ -0,0 +1,23 @@
+import { Activity } from "gh-recent-activity";
+
+const RecentActivity = ({
+  activity,
+  show,
+}: {
+  activity: Activity[];
+  show: boolean;
+}) => {
+  if (!show) return null;
+  return (
+    <>
+      <h3>Recent Activity</h3>
+      <ul className="recent-activity-list">
+        {activity?.map((act, idx) => (
+          <li key={`${act.type}-${idx}`}>{act.text}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default RecentActivity;
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,10 +9,12 @@ import { serialize } from "next-mdx-remote/serialize";
 import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
 import { getUserActivity, Activity } from "gh-recent-activity";
 
+import defaults from "../constants/default.json";
 import Container from "../components/Container";
 import Head from "../components/_head";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import RecentActivity from "../components/RecentActivity";
 import { useTheme } from "../context/ThemeContext";
 
 interface HomeProps {
@@ -25,14 +27,16 @@ interface HomeProps {
 const Home: NextPage<HomeProps> = (props) => {
   const { homeContent } = props;
   const { theme } = useTheme();
-  const [recentActivity, setRecentActivity] = useState<Activity>();
+  const [recentActivity, setRecentActivity] = useState<Activity[]>();
+
+  const { show_recent_activity: showRecentActivity } = defaults;
 
   useEffect(() => {
-    getUserActivity("akulsr0", { includeEmoji: true }).then(
-      (resp: Activity) => {
+    if (showRecentActivity) {
+      getUserActivity("akulsr0", { includeEmoji: true }).then((resp) => {
         setRecentActivity(resp);
-      }
-    );
+      });
+    }
   }, []);
 
   return (
@@ -42,8 +46,8 @@ const Home: NextPage<HomeProps> = (props) => {
       <main className="main-content">
         <MDXRemote
           {...homeContent}
-          components={{ Link, Image }}
-          scope={{ theme, recentActivity }}
+          components={{ Link, Image, RecentActivity }}
+          scope={{ theme, showRecentActivity, recentActivity }}
         />
       </main>
       <Footer />
