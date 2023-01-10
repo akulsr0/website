e1e0cbeeeb8072d9e59e1bdcdef228faa31fba4b
Akul Srivastava <akulsr0@gmail.com>
2023-01-10 18:57:08 +0530

    fix: deploy failure

--- components/PageViews.tsx
+++ components/PageViews.tsx
@@ -6,7 +6,7 @@ function PageViews(props: { type: ContentType; slug: string }) {
   const { type, slug } = props;
   const isProd = isProduction();
 
-  if (isProd) return [];
+  if (isProd) return null;
 
   return (
     <>
