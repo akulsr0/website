a174f32e47bdabffbb3524fd50ba56e0ef178175
Akul Srivastava <akulsr0@gmail.com>
2023-01-11 16:52:08 +0530

    chore(service): api for devtips and blogs list (#55)
    * chore(service): api for devtips and blogs list
    
    * chore: try

---
+++ pages/api/blogs.ts
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import blogs from "../../content/blogs.json";
+
+export default function getBlogsList(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  res.json({ data: blogs, success: true });
+}
